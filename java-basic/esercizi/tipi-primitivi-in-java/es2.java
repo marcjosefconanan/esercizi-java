f71a43f3b9d77a3b72a7972d4942c5381320ce9e
marc josef conanan <116675081+marcjosefconanan@users.noreply.github.com>
2023-09-25 18:15:21 +0200

    Add files via upload

---
+++ java-basic/esercizi/tipi-primitivi-in-java/es2.java
@@ -0,0 +1,11 @@
+package Lezione1;
+
+// char to string
+public class es2 {
+    public static void main(String[] args) {
+        char x = 'A';
+        char y = 'B';
+
+        System.out.println("La stringa è " + x + y);
+    }
+}
