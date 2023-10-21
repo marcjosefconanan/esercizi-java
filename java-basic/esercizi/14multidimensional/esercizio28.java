7af9917c33f766c30045dd75b3b4f021beb4936d
Marc Josef Conanan <116675081+marcjosefconanan@users.noreply.github.com>
2023-10-21 11:38:57 +0200

    Add files via upload

--- java-basic/esercizi/14multidimensional/esercizio28.java
+++ java-basic/esercizi/14multidimensional/esercizio28.java
@@ -8,46 +8,69 @@
  //2 5
  //3 6
 
- public class esercizio28 {
-     public static void main(String[] args) {
-         //int[][] matrice = {{1, 2, 3}, {4, 5, 6}};
-         //int[][] matrice = {{1, 2, 3,4}, {4, 5, 6}};
+//il problema sussiste ancora, cosa succede con una "matrice" del genere: int[][] matrice = {{1, 2, 3,4,5}, {4, 5, 6,7}};
 
-         int[][] matrice = {{1, 2, 3,4}, {4, 5, 6,7}}; //dimensioni 2 x 4 possono funzionare.
-         // se metto int[][] matrice = {{1, 2, 3, 4}, {4, 5, 6}};
-         //il codice non funziona xke il quarto intero nel primo matrice va oltre l'altro
+import java.io.StringReader;
 
-         System.out.println("matrice di partenza: ");
-         stampaMatrice(matrice);
+public class esercizio28 {
+    public static void main(String[] args) {
+        //int[][] matrice = {{1, 2, 3}, {4, 5, 6}};
+        //int[][] matrice = {{1, 2, 3,4}, {4, 5, 6}};
 
-         int[][] matriceScambiata = switchRigheColonne(matrice);
+        int[][] matrice = {{1, 2, 3}, {4, 5, 6}}; //dimensioni 2 x 4 possono funzionare.
+        // se metto int[][] matrice = {{1, 2, 3, 4}, {4, 5, 6}};
+        //il codice non funziona xke il quarto intero nel primo matrice va oltre l'altro
 
-         System.out.println("Matrice risultato:");
-         stampaMatrice(matriceScambiata);
-     }
-     public static int[][] switchRigheColonne(int [][] matrice){
+        System.out.println("matrice di partenza: ");
+        stampaMatrice(matrice);
 
-         int righe = matrice.length;
-         int colonne = matrice[0].length;
+        int[][] matriceScambiata = switchRigheColonne(matrice);
 
-         int [][] nuovoMatrice = new int [colonne][righe];
+        System.out.println("Matrice risultato:");
+        stampaMatrice(matriceScambiata);
+        System.out.println(isvalidMatrix(matrice));
+    }
 
-         for (int i=0; i < righe; i++){
-             for (int j = 0; j < colonne; j++){
-                 nuovoMatrice[j][i] = matrice[i][j];
-             }
-         }
+    public static boolean isvalidMatrix(int[][] matrice) {
+        boolean flag = false;
+        int[] array1 = matrice[0];
+        for (int i = 0; i < matrice.length; i++) {
+            if (array1.length == matrice[i].length) {
+                flag = true;
+            } else {
+                flag = false;
+            }
+        }
+        return flag;
+    }
 
-         return nuovoMatrice;
-     }
+    public static int[][] switchRigheColonne(int[][] matrice) {
+        if (isvalidMatrix(matrice) == true) {
 
-     public  static void  stampaMatrice(int[][] matrice){
-         for (int[] riga: matrice) {
-             for (int element : riga) {
-                 System.out.print(element + "");
-             }
-             System.out.println();
-         }
-     }
-}
+            int righe = matrice.length;
+            int colonne = matrice[0].length;
+
+            int[][] nuovoMatrice = new int[colonne][righe];
+
+            for (int i = 0; i < righe; i++) {
+                for (int j = 0; j < colonne; j++) {
+                    nuovoMatrice[j][i] = matrice[i][j];
+                }
+            }
+            return nuovoMatrice;
+        }
+        return matrice;
+    }
+
+
+        public static void stampaMatrice ( int[][] matrice){
+            for (int[] riga : matrice) {
+                for (int element : riga) {
+                    System.out.print(element + "");
+                }
+                System.out.println();
+            }
+        }
+    }
+
 
